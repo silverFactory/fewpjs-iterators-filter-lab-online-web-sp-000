648a43c8938d4fd0de5ed5bc33ba6cf8012f50ff
Will Studabaker <will.studabaker@gmail.com>
2021-02-11 16:05:58 -0500

    commit tree 9a12377e8211820426c86739a3467dd31d4209bd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(array, match){
+  return array.filter(element => {return element.toLowerCase() == match.toLowerCase()})
+}
+function fuzzyMatch(array, match){
+  return array.filter(element => {return element.slice(0, match.length) == match})
+}
+function matchName(drivers, match){
+  return drivers.filter(driver => {return driver.name == match})
+}
